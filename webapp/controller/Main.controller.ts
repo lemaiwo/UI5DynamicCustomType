d6591afe98d42b95a2b68a81fb068060d9645930
Wouter.LEMAIRE ext.ec.europa.eu <lemaiwo>
2023-05-11 10:06:49 +0000

    add example

--- webapp/controller/Main.controller.ts
+++ webapp/controller/Main.controller.ts
@@ -3,6 +3,8 @@ import BaseController from "./BaseController";
 import formatter from "../model/formatter";
 import JSONModel from "sap/ui/model/json/JSONModel";
 import CustomType from "../utils/CustomType";
+import HBox from "sap/m/HBox";
+import Input from "sap/m/Input";
 
 /**
  * @namespace be.wl.myapp.controller
@@ -12,6 +14,8 @@ export default class Main extends BaseController {
 	public onInit():void{
 		const customType = CustomType;
 		this.getView().setModel(new JSONModel({value:"hello"}));
+		const inp = new Input({value:"{path:'value',type:'be.wl.myapp.utils.CustomType'}"});
+		(this.byId("placeholder") as HBox).insertItem(inp,1)
 	}
 
 	public sayHello() : void {
