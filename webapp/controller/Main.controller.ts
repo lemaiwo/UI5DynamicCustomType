2cf7539074720f61836023bed7b30e5da36b9b5b
Wouter.LEMAIRE ext.ec.europa.eu <lemaiwo>
2023-05-11 09:20:10 +0000

    load type

--- webapp/controller/Main.controller.ts
+++ webapp/controller/Main.controller.ts
@@ -1,12 +1,18 @@
 import MessageBox from "sap/m/MessageBox";
 import BaseController from "./BaseController";
 import formatter from "../model/formatter";
+import JSONModel from "sap/ui/model/json/JSONModel";
+import CustomType from "../utils/CustomType";
 
 /**
  * @namespace be.wl.myapp.controller
  */
 export default class Main extends BaseController {
 	private formatter = formatter;
+	public onInit():void{
+		const customType = CustomType;
+		this.getView().setModel(new JSONModel({value:"hello"}));
+	}
 
 	public sayHello() : void {
 		MessageBox.show("Hello World!");
